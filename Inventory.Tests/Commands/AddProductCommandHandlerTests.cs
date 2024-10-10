912a01063f029145ba14af3407ff8a568939c4f3
Jeisson Juanias <wall9169@gmail.com>
2024-10-10 13:35:51 -0500

    Creating the Inventory.test project

---
+++ Inventory.Tests/Commands/AddProductCommandHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Inventory.Application.Commands;
+using Inventory.Application.DTOs;
+using Inventory.Application.Handlers;
+using Inventory.Domain.Entities;
+using Inventory.Domain.Interfaces;
+using Moq;
+
+public class AddProductCommandHandlerTests
+{
+    private readonly Mock<IProductCommandRepository> _productRepositoryMock;
+    private readonly AddProductCommandHandler _handler;
+
+    public AddProductCommandHandlerTests()
+    {
+        _productRepositoryMock = new Mock<IProductCommandRepository>();
+        _handler = new AddProductCommandHandler(_productRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldAddProduct_WhenCommandIsValid()
+    {
+        // Arrange
+        var productDto = new ProductDto
+        {
+            Name = "Test Product",
+            CategoryId = 1,
+            Price = 10.0m
+        };
+
+        var command = new AddProductCommand(productDto);
+
+        _productRepositoryMock
+            .Setup(r => r.AddAsync(It.IsAny<Product>()));
+
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<ResponseResult>(); // Cambia esto según lo que devuelva tu handler
+        _productRepositoryMock.Verify(r => r.AddAsync(It.IsAny<Product>()), Times.Once);
+    }
+}
